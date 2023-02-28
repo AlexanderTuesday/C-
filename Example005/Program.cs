b589e0c1331a45dd643441920cf0c10a6a316a9b
AVtor <vtorik1@mail.ru>
2023-02-28 13:06:12 +0300

    added 005

---
+++ Example005/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя пользователя");
+string username = Console.ReadLine();
+
+if(username == "Galina")
+{
+    Console.WriteLine("Ура, это же Galina");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
